6a8b9c7a80444083337e023fab9c275c37445e96
Alberto Pedraza <washburnuser@gmail.com>
2026-09-07 16:54:00 -0600

    Ahora sí ya está el catálogo con las imágenes anexadas en el app.js

--- app.js
+++ app.js
@@ -28,40 +28,40 @@
       id: 'varitas', label: 'Varitas', icon: 'wand',
       products: [
         { id: 'var1', name: 'Varita de Colmillo de Serpiente', price: '58', imagen:'assets/Varita_Colmillo_Serpiente.png', desc: 'Madera de ébano con núcleo de colmillo de serpiente. Favorece hechizos defensivos', cant: '10'},
-        {id: 'var2', name: 'Varita de Vena de Basilisco', price: '75', imagen:'assets/Varita_Colmillo_Serpiente.png', desc: 'Madera de serpiente con núcleo de cuerno de basilisco. Canaliza un poder legendario, estatus supremo y elegancia excepcional', cant: '10'},
-        { id: 'var3', name: 'Varita de Salazar Slytherin', price: '100', imagen:'assets/Varita_Colmillo_Serpiente.png', desc: 'Madera de ébano con núcleo de colmillo de serpiente. Favorece hechizos defensivos', cant: '10'}
+        {id: 'var2', name: 'Varita de Vena de Basilisco', price: '75', imagen:'assets/Varita_Vena_de_Basilisco.png', desc: 'Madera de serpiente con núcleo de cuerno de basilisco. Canaliza un poder legendario, estatus supremo y elegancia excepcional', cant: '10'},
+        { id: 'var3', name: 'Varita de Salazar Slytherin', price: '100', imagen:'assets/Salazar_Slytherin_Wand.png', desc: 'Madera de ébano con núcleo de colmillo de serpiente. Favorece hechizos defensivos', cant: '10'}
       ]
     },
     {
       id: 'pociones', label: 'Pociones y Elixirs', icon: 'plush',
       products: [
-        { id: 'poc1', name: 'Serpent’s Focus', price: '12', imagen:'assets/Varita_Colmillo_Serpiente.png', desc: 'Mejora temporalmente la concentración del mago.', cant: '10'},
-        {id: 'poc2', name: 'Veritaserum', price: '25', imagen:'assets/Varita_Colmillo_Serpiente.png', desc: 'Un poderoso suero de la verdad. Tres gotas son suficientes para revelar los más íntimos secretos de cualquier mago.', cant: '10' },
-        { id: 'poc3', name: 'Felix Felicis', price: '50', imagen:'assets/Varita_Colmillo_Serpiente.png', desc: 'Proporciona al bebedor un periodo de buena suerte durante el cual, todo lo que intente le saldrá bien.', cant: '10'}
+        { id: 'poc1', name: 'Serpent’s Focus', price: '12', imagen:'assets/Serpents_focus.png', desc: 'Mejora temporalmente la concentración del mago.', cant: '10'},
+        {id: 'poc2', name: 'Veritaserum', price: '25', imagen:'assets/Veritaserum.png', desc: 'Un poderoso suero de la verdad. Tres gotas son suficientes para revelar los más íntimos secretos de cualquier mago.', cant: '10' },
+        { id: 'poc3', name: 'Felix Felicis', price: '50', imagen:'assets/Felix_Felicis.png', desc: 'Proporciona al bebedor un periodo de buena suerte durante el cual, todo lo que intente le saldrá bien.', cant: '10'}
       ]
     },
     {
       id: 'tunicas', label: 'Túnicas y Ropa', icon: 'robe',
       products: [
-        {id: 'tuni1', name: 'Túnica Clásica de Slytherin', price: '35', imagen:'assets/Varita_Colmillo_Serpiente.png', desc: 'Túnica negra con detalles verde esmeralda', cant: '10' },
-        { id: 'tuni2', name: 'Túnica de la Reliquia de la Casa', price: '65', imagen:'assets/Varita_Colmillo_Serpiente.png', desc: 'Una bata de casa, para los más astutos y ambiciosos de Slytherin', cant: '10' },
-        { id: 'tuni3', name: 'Conjunto de Bufanda, Guantes y Gorro de la Casa', price: '15', imagen:'assets/Varita_Colmillo_Serpiente.png', desc: 'Accesorios ideales para época de invierno.', cant: '10' }
+        {id: 'tuni1', name: 'Túnica Clásica de Slytherin', price: '35', imagen:'assets/tunica1.png', desc: 'Túnica negra con detalles verde esmeralda', cant: '10' },
+        { id: 'tuni2', name: 'Túnica de la Reliquia de la Casa', price: '65', imagen:'assets/tunica2.png', desc: 'Una bata de casa, para los más astutos y ambiciosos de Slytherin', cant: '10' },
+        { id: 'tuni3', name: 'Conjunto de Bufanda, Guantes y Gorro de la Casa', price: '15', imagen:'assets/gorros.png', desc: 'Accesorios ideales para época de invierno.', cant: '10' }
       ]
     },
     {
       id: 'joyeria', label: 'Reliquias Familiares', icon: 'ring',
       products: [
-        {id: 'reli1', name: 'Anillo de la Serpiente Plateada', price: '70', imagen:'assets/Varita_Colmillo_Serpiente.png', desc: 'Anillo antiguo utilizado como símbolo entre familias mágicas.', cant: '10' },
-        { id: 'reli2', name: 'Reloj de serpiente de bolsillo', price: '55', imagen:'assets/Varita_Colmillo_Serpiente.png', desc: 'Reloj encantado que nunca pierde la hora.', cant: '10' },
-        { id: 'reli3', name: 'Guardapelo de Slytherin', price: '100', imagen:'assets/Varita_Colmillo_Serpiente.png', desc: 'Este guardapelo dorado es una reliquia familiar de Slytherin, perteneció en un principio a Salazar Slytherin y acabó en manos de la familia Gaunt.', cant: '1' }
+        {id: 'reli1', name: 'Anillo de la Serpiente Plateada', price: '70', imagen:'assets/Anillo_Serpiente_Plateada.png', desc: 'Anillo antiguo utilizado como símbolo entre familias mágicas.', cant: '10' },
+        { id: 'reli2', name: 'Reloj de serpiente de bolsillo', price: '55', imagen:'assets/Reloj_serpiente_bolsillo.png', desc: 'Reloj encantado que nunca pierde la hora.', cant: '10' },
+        { id: 'reli3', name: 'Guardapelo de Slytherin', price: '100', imagen:'assets/Guardapelo_Slytherin.png', desc: 'Este guardapelo dorado es una reliquia familiar de Slytherin, perteneció en un principio a Salazar Slytherin y acabó en manos de la familia Gaunt.', cant: '1' }
       ]
     },
     {
       id: 'libros', label: 'Grimorios, Libros y Documentos Secretos', icon: 'book',
       products: [
-        {id: 'book1', name: 'Apuntes de Campo Sobre la Lengua de Serpiente', price: '60', imagen:'assets/Varita_Colmillo_Serpiente.png', desc: 'Libro excepcional sobre los descubrimientos de S. S. en el estudio de la lengua de serpiente ', cant: '10' },
-        { id: 'book2', name: 'Diario de Slytherin', price: '100', imagen:'assets/Varita_Colmillo_Serpiente.png', desc: 'En este diario se abordan los desacuerdos de Slytherin con los fundadores de Hogwarts, y ofrece una visión de sus planes del futuro', cant: '10' },
-        { id: 'book3', name: 'Archivo de Genealogía de Sangre Pura', price: '46', imagen:'assets/Varita_Colmillo_Serpiente.png', desc: 'Compendio reservado de linajes mágicos, alianzas familiares y registros heráldicos de antiguas casas', cant: '10' }
+        {id: 'book1', name: 'Apuntes de Campo Sobre la Lengua de Serpiente', price: '60', imagen:'assets/libro1.png', desc: 'Libro excepcional sobre los descubrimientos de S. S. en el estudio de la lengua de serpiente ', cant: '10' },
+        { id: 'book2', name: 'Diario de Slytherin', price: '100', imagen:'assets/libro2.png', desc: 'En este diario se abordan los desacuerdos de Slytherin con los fundadores de Hogwarts, y ofrece una visión de sus planes del futuro', cant: '10' },
+        { id: 'book3', name: 'Archivo de Genealogía de Sangre Pura', price: '46', imagen:'assets/libro3.png', desc: 'Compendio reservado de linajes mágicos, alianzas familiares y registros heráldicos de antiguas casas', cant: '10' }
       ]
     }
   ];
